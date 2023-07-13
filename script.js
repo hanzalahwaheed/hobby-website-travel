8c31dcfaf49137e71ade5fa0f32c48f3b3b60d61
Hanzalah Waheed <101456741+hanzalahwaheed@users.noreply.github.com>
2023-07-14 02:35:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Change slide every 5 seconds
+var slides = document.querySelectorAll('.slide');
+var currentSlide = 0;
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(nextSlide, 3500);
